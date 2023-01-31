4f9679705ed36a813e27f32e8f175be57536d067
alexey.kurch <Alexey.Kurch@itechart-group.com>
2023-01-31 17:56:45 +0200

    CarN - Style Cards + Add Material Theme

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
 
 import { DashboardContainerComponent } from "./dashboard-container.component";
 import { DashboardRoutingModule } from './dashboard-routing.module';
@@ -7,6 +9,6 @@ import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
   declarations: [DashboardComponent, DashboardContainerComponent],
-  imports: [CommonModule, DashboardRoutingModule],
+  imports: [CommonModule, DashboardRoutingModule, MatCardModule, MatButtonModule],
 })
 export class DashboardModule {}
